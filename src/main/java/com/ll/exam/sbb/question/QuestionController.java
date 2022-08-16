ac193be20770dda9b73813415fe291dc18d67cac
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-16 13:16:38 +0900

    상세파이지 작업

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.ll.exam.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,12 +17,20 @@ public class QuestionController {
     @RequestMapping("/question/list")
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String list(Model model) {
-        List<Question> questionList = questionService.findAll();
+        List<Question> questionList = questionService.getList();
 
         // 미래에 실행된 question_list.html 에서
         // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
         model.addAttribute("questionList", questionList);
 
         return "question_list";
+    }
+
+    @RequestMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable int id) {
+        Question question = questionService.getQuestion(id);
+
+        model.addAttribute(question);
+        return "question_detail";
     }
 }
--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.ll.exam.sbb.question;
 
+import com.ll.exam.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +13,18 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> findAll() {
+    public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(int id) {
+        Optional<Question> oq = questionRepository.findById(id);
+
+        // return oq.orElseThrow(DataNotFoundException); // 만약에 없으면 예외를 발생시켜라
+
+        if (oq.isPresent()) {
+            return oq.get();
+        }
+        throw new DataNotFoundException("question not found"); // 실행이 되면 더 이상 진행하지 못함
     }
 }
