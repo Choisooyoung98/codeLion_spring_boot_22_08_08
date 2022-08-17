24d37768dff10b908800184a3ff43bc974eec412
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-17 15:34:39 +0900

    answer validation 적용, form_error 분리, navbar 분리

--- src/main/java/com/ll/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -5,9 +5,11 @@ import com.ll.exam.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/answer")
@@ -19,10 +21,15 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @RequestMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable int id, String content) {
+    public String createAnswer(Model model, @PathVariable int id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
 
-        answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+
+        answerService.create(question, answerForm.getContent());
 
         return "redirect:/question/detail/%d".formatted(id);
     }
